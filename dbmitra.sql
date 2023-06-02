81ebde00869111705adb3ec80aa998d563fd37bb
tiansetiaone <107497446+tiansetiaone@users.noreply.github.com>
2023-06-02 13:24:58 +0700

    Add files via upload

--- dbmitra.sql
+++ dbmitra.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 01, 2023 at 03:51 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.0.25
+-- Host: localhost
+-- Generation Time: Jun 02, 2023 at 06:22 AM
+-- Server version: 10.5.20-MariaDB
+-- PHP Version: 7.3.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `dbmitra`
+-- Database: `id20853889_host`
 --
 
 -- --------------------------------------------------------
@@ -39,11 +39,11 @@ CREATE TABLE `m_barang` (
 --
 
 INSERT INTO `m_barang` (`id`, `kode`, `nama`, `harga`) VALUES
-(1, 'A001', 'Barang A', '200000'),
-(2, 'C025', 'Barang B', '350000'),
-(3, 'A102', 'Barang C', '125000'),
-(4, 'A301', 'Barang D', '300000'),
-(5, 'B221', 'Barang E', '300000');
+(1, 'A001', 'Barang A', 200000),
+(2, 'C025', 'Barang B', 350000),
+(3, 'A102', 'Barang C', 125000),
+(4, 'A301', 'Barang D', 300000),
+(5, 'B221', 'Barang E', 300000);
 
 -- --------------------------------------------------------
 
@@ -58,13 +58,20 @@ CREATE TABLE `m_customer` (
   `telp` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `m_customer`
+--
+
+INSERT INTO `m_customer` (`id`, `kode`, `name`, `telp`) VALUES
+(13, '001', 'Tian Ahmad Setiawan', '081382221886');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `t_sales`
+-- Table structure for table `t_saless`
 --
 
-CREATE TABLE `t_sales` (
+CREATE TABLE `t_saless` (
   `id` int(10) NOT NULL,
   `kodex` varchar(15) NOT NULL,
   `tgl` date NOT NULL,
@@ -73,19 +80,15 @@ CREATE TABLE `t_sales` (
   `subtotal` decimal(10,0) NOT NULL,
   `diskon` decimal(10,0) NOT NULL,
   `ongkir` decimal(10,0) NOT NULL,
-  `total_bayar` decimal(10,0) NOT NULL,
-  `sales_id` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `total_bayar` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `t_sales`
+-- Dumping data for table `t_saless`
 --
 
-INSERT INTO `t_sales` (`id`, `kodex`, `tgl`, `cust_id`, `jumlah`, `subtotal`, `diskon`, `ongkir`, `total_bayar`, `sales_id`) VALUES
-(38, '202305-0005', '2023-05-19', 3, 25, '7200000', '70000', '11000', '7119000', 0),
-(39, '202305-0006', '2023-05-20', 5, 25, '7200000', '70000', '5000', '7125000', 0),
-(40, '202305-0007', '2023-05-12', 5, 25, '7200000', '50000', '11000', '7139000', 0),
-(41, '202306-0008', '2023-06-01', 3, 15, '4050000', '5000', '1000', '4044000', 0);
+INSERT INTO `t_saless` (`id`, `kodex`, `tgl`, `cust_id`, `jumlah`, `subtotal`, `diskon`, `ongkir`, `total_bayar`) VALUES
+(1, '202306-0001', '2023-06-02', 1, 10, 2250000, 5000, 1000, 2244000);
 
 -- --------------------------------------------------------
 
@@ -109,7 +112,8 @@ CREATE TABLE `t_sales_dett` (
 --
 
 INSERT INTO `t_sales_dett` (`sales_id`, `kode`, `harga_bandrol`, `qty`, `diskon_pct`, `diskon_nilai`, `harga_diskon`, `total`) VALUES
-(21, 'A001', '200000', 5, '10', '20000', '180000', '900000');
+(21, 'A001', 200000, 5, 10, 20000, 180000, 900000),
+(25, 'B221', 300000, 5, 10, 30000, 270000, 1350000);
 
 --
 -- Indexes for dumped tables
@@ -129,13 +133,11 @@ ALTER TABLE `m_customer`
   ADD KEY `id` (`id`);
 
 --
--- Indexes for table `t_sales`
+-- Indexes for table `t_saless`
 --
-ALTER TABLE `t_sales`
-  ADD PRIMARY KEY (`id`,`cust_id`,`sales_id`),
-  ADD KEY `cust_id` (`cust_id`),
-  ADD KEY `kodex` (`kodex`),
-  ADD KEY `sales_id` (`sales_id`);
+ALTER TABLE `t_saless`
+  ADD PRIMARY KEY (`id`,`cust_id`),
+  ADD KEY `kodex` (`kodex`,`cust_id`);
 
 --
 -- Indexes for table `t_sales_dett`
@@ -158,19 +160,19 @@ ALTER TABLE `m_barang`
 -- AUTO_INCREMENT for table `m_customer`
 --
 ALTER TABLE `m_customer`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT for table `t_sales`
+-- AUTO_INCREMENT for table `t_saless`
 --
-ALTER TABLE `t_sales`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+ALTER TABLE `t_saless`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `t_sales_dett`
 --
 ALTER TABLE `t_sales_dett`
-  MODIFY `sales_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `sales_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
